4860e6d6c61d8ac4f0624ed1202273c9d2b8cab1
tyaBEe <vit102@naver.com>
2025-06-10 19:14:27 +0900

    mypage 수정

--- moDohae/routes/users.js
+++ moDohae/routes/users.js
@@ -1,7 +1,6 @@
 // route/user.js
 const express = require('express');
-const { sendAuthEmail, verifyAuthCode } = require('../controllers/userController');
-const { signup, login, logout } = require('../controllers/userController');
+const { signup, login, logout, sendAuthEmail, verifyAuthCode, getUserInfo, updateUserInfo } = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -10,7 +9,7 @@ router.post('/signup', signup);
 router.post('/logout', logout);
 router.post('/send-auth-email', sendAuthEmail);
 router.post('/verify-auth-code', verifyAuthCode);
-router.get('/info', userController.getUserInfo);
-router.put('/info', userController.updateUserInfo);
+router.get('/info', getUserInfo);
+router.put('/info', updateUserInfo);
 
 module.exports = router;
